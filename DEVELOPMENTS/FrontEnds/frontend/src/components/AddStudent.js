9d766fc2e39a19ed40ed7e240467b5c2b0556a84
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2023-02-24 05:35:09 +0300

    FIn de Projet Echec

--- DEVELOPMENTS/FrontEnds/frontend/src/components/AddStudent.js
+++ DEVELOPMENTS/FrontEnds/frontend/src/components/AddStudent.js
@@ -1,105 +1,122 @@
 // import { type } from '@testing-library/user-event/dist/type';
-// import axios from 'axios';
+import axios from 'axios';
 import React from 'react';
 
 class Ajout extends React.Component{
      state = {
-        nom:'',
-        prenom:'',
-        niveau:'',
-        adresse:'',
-        date_de_naissance:'',
-        code_postal:'',
-        city:'',
-        country:'',
-        quartier:'',
-        cv:'',
+        // Etudiant_FirstName:'',
+        // Etudiant_LastName:'',
+        // Etudiant_Genre:'',
+        // Etudiant_Birth:'',
+        // Code_Postal:'',
+        // Adress_City:'',
+        // Adress_country:'',
+        // Adress_Quartier:'',
+        // Etudiant_Photo:'',
+        // cv:'',
+        Etudiant_FirstName: '',
+        Etudiant_LastName: '',
+        Etudiant_Genre: '',
+        Etudiant_Birth: '',
+        Etudiant_Photo: '',
+        Etudiant_Adress: [{
+          Adress_City: '',
+          Adress_Country: '',
+          Adress_Quartier: '',
+          Code_Postal: ''}],
         redirect:false,
         errors:[]
      }
      handleCode_PostalChange=(e)=>{
-        this.setState({code_postal: e.target.value})
+        this.setState({Code_Postal: e.target.value})
      }
      handleCityChange=(e)=>{
-        this.setState({city: e.target.value})
+        this.setState({Adress_City: e.target.value})
      }
      handleCountryChange=(e)=>{
-        this.setState({country: e.target.value})
+        this.setState({Adress_Country: e.target.value})
      }
      handleQuartierChange=(e)=>{
-        this.setState({quartier: e.target.value})
+        this.setState({Adress_Quartier: e.target.value})
      }
-    handleCvchange=(e)=>{
-        this.setState({cv:e.target.value})
+    handleGenreChange=(e)=>{
+       this.setState({Etudiant_Genre:e.target.value})
     }
     handleNameChange=(e)=>{
-        this.setState({nom:e.target.value})
+        this.setState({Etudiant_LastName:e.target.value})
     }
     handlePrenomChange=(e)=>{
-        this.setState({prenom:e.target.value})
+        this.setState({Etudiant_FirstName:e.target.value})
     }
-    handleNiveauChange=(e)=>{
-        this.setState({niveau:e.target.value})
+    handlePhotoChange=(e)=>{
+        this.setState({Etudiant_Photo:e.target.value})
     }
-    handleAdresseChange=(e)=>{
-        this.setState({adresse:e.target.value})
-    }
-    handleDate_de_NaissanceChange=(e)=>{
-        this.setState({date_de_naissance:e.target.value})
-    }
-    // handlesubmit=(e)=>{
-    //     e.preventDefault()
-    //     console.log('Created successfully')
-    //     let bodyFormData= new FormData()
-    //     bodyFormData.set('nom',this.state.nom)
-    //     bodyFormData.set('prenom',this.state.prenom)
-    //     bodyFormData.set('niveau',this.state.niveau)
-    //     bodyFormData.set('adresse',this.state.adresse)
-    //     bodyFormData.set('date_de_naissance',this.state.date_de_naissance)
-    //     bodyFormData.set('code_postal',this.state.code_postal)
-    //     bodyFormData.set('city',this.state.city)
-    //     bodyFormData.set('country',this.state.country)
-    //     bodyFormData.set('quartier',this.state.quartier)
-    //     bodyFormData.set('cv',this.state.cv)
-    
-    // axios.post('',bodyFormData)
-    // .then(res=>{
-    //     this.setState({redirect: true})
-    // })
-    // .catch(error=>{
-    //     if(error.response.status===401){
-    //         this.setState({errors:error.response.data.errors},()=>console.log(this.state))
-    //     }
-    // })
+    // handleNiveauChange=(e)=>{
+    //     this.setState({niveau:e.target.value})
     // }
+    // handleAdresseChange=(e)=>{
+    //     this.setState({adresse:e.target.value})
+    // }
+    handleDate_de_NaissanceChange=(e)=>{
+        this.setState({Etudiant_Birth:e.target.value})
+    }
+    handlesubmit=(e)=>{
+        e.preventDefault()
+        let bodyFormData= new FormData()
+        bodyFormData.set('Etudiant_FirstName',this.state.Etudiant_FirstName)
+        bodyFormData.set('Etudiant_LastName',this.state.Etudiant_LastName)
+        bodyFormData.set('Etudiant_Genre',this.state.Etudiant_Genre)
+        //bodyFormData.set('adresse',this.state.adresse)
+        bodyFormData.set('Etudiant_Birth',this.state.Etudiant_Birth)
+        bodyFormData.set('Code_Postal',this.state.Etudiant_Adress.Code_Postal)
+        bodyFormData.set('Adress_City',this.state.Etudiant_Adress.Adress_City)
+        bodyFormData.set('Adress_Country',this.state.Etudiant_Adress.Adress_Country)
+        bodyFormData.set('Adress_Quartier',this.state.Etudiant_Adress.Adress_Quartier)
+        bodyFormData.set('Etudiant_Photo',this.state.Etudiant_Photo)
+        //bodyFormData.set('cv',this.state.cv)
+    
+    axios.post('https://localhost:44338/api/etudiants/add',bodyFormData)
+    .then(res=>{
+        this.setState({redirect: true},
+            console.log('inséré'))
+    })
+    .catch(error=>{
+        if(error.response.status===401){
+            this.setState({errors:error.response.data.errors},()=>console.log(this.state))
+        }
+    })
+}
     render(){
+    if (this.state.redirect){
+        return(<redirect to="/"/>)}
     return (
         <div>
             <form method="post" onSubmit={this.handlesubmit}>
-            <label htmlFor='nom'>Nom</label>
-            <input type="text" name="nom" required onChange={this.handleNameChange}></input>
-            <label htmlFor='prenom'>Prenom</label>
-            <input type="text" name="prenom" required onChange={this.handlePrenomChange}></input>
-            <label htmlFor='niveau'>Niveau</label>
-            <input type="text" name="niveau" required onChange={this.handleNiveauChange}></input>
-            <label htmlFor='adresse'>Adresse</label>
-            <input type="text" name="adresse" required onChange={this.handleAdresseChange}></input>
-            <label htmlFor='date_de_naissance'>Date de naissance</label>
-            <input type="date" name="date_de_naissance" required onChange={this.handleDate_de_NaissanceChange}></input>
-            <label htmlFor='quartier'>Adresse respectif</label>
+            <label htmlFor='Etudiant_LastName'>Nom</label>
+            <input type="text" name="Etudiant_LastName" required onChange={this.handleNameChange}></input>
+            <label htmlFor='Etudiant_FirstName'>Prenom</label>
+            <input type="text" name="Etudiant_FirstName" required onChange={this.handlePrenomChange}></input>
+            {/* <label htmlFor='niveau'>Niveau</label>
+            <input type="text" name="niveau" required onChange={this.handleNiveauChange}></input> */}
+            {/* <label htmlFor='adresse'>Adresse</label>
+            <input type="text" name="adresse" required onChange={this.handleAdresseChange}></input> */}
+            <label htmlFor='Etudiant_Birth'>Date de naissance</label>
+            <input type="date" name="Etudiant_Birth" required onChange={this.handleDate_de_NaissanceChange}></input>
+            <label htmlFor='Adress_Quartier'>Adresse respectif</label>
             <input type="text" name="quartier" required onChange={this.handleQuartierChange}></input>
-            <label htmlFor='code_postal'>Code postal</label>
-            <input type="text" name="code_postal" required onChange={this.handleCode_PostalChange}></input>
-            <label htmlFor='city'>Ville</label>
-            <input type="text" name="city" required onChange={this.handleCityChange}></input>
-            <label htmlFor='country'>Pays</label>
-            <input type="text" name="country" required onChange={this.handleCountryChange}></input>
-            <label htmlFor='cv'>Cv</label>
-            <input type="file" name='cv'id='cv' onChange={this.handleCvchange}></input>
+            <label htmlFor='Code_Postal'>Code postal</label>
+            <input type="text" name="Code_Postal" required onChange={this.handleCode_PostalChange}></input>
+            <label htmlFor='Adress_City'>Ville</label>
+            <input type="text" name="Adress_City" required onChange={this.handleCityChange}></input>
+            <label htmlFor='Adress_Country'>Pays</label>
+            <input type="text" name="Adress_Country" required onChange={this.handleCountryChange}></input>
+            <label htmlFor='Etudiant_Genre'>Genre</label>
+            <input type="text" name='Etudiant_Genre' onChange={this.handleGenreChange}></input>
+            <label htmlFor='Etudiant_Photo'>Photo</label>
+            <input type="text" name='Etudiant_Photo' onChange={this.handlePhotoChange}></input>
             <button type='submit'>Valider</button>
             </form>
-           
-            </div>
+           </div>
       )
 }
 }
--- DEVELOPMENTS/FrontEnds/frontend/src/components/StudentList.js
+++ DEVELOPMENTS/FrontEnds/frontend/src/components/StudentList.js
@@ -1,45 +1,55 @@
 import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 function StudentList() {
-    var Students = [
-        {
-            nom:'ABOU',
-            prenom:'BABDOU'
-        },
-        {
-            nom:'DIDOU',
-            prenom:'COUCOU'
-        },
-        {
-            nom:'BIBOU',
-            prenom:'GOUGOUL'
-        }
-    ]
-    const mapped=Students.map(elm => {
-        return (
+  const [etudiantAdress, setEtudiantAdress] = useState([]);
+  var Etudiants=[];
+
+  useEffect(() => {
+    axios.get('https://localhost:44338/api/etudiants')
+      .then(response => setEtudiantAdress(response.data))
+      .catch(error => console.error(error));
+  }, []);
+  const mapped=Etudiants.map(elm => {
+    return (
+            <tr>
+            <td>{elm.Etudiants_FirstName}</td>
+            <td>{elm.Etudiants_LastName}</td>
+            <td>{elm.Etudiant_Genre}</td>
+            <td>{elm.Etudiant_Birth}</td>
+            <td>{elm.Etudiant_Photo}</td>   
+            </tr>
             
-                <tr>
-                <td>{elm.nom}</td>
-                <td>{elm.prenom}</td>
-                <td><button type="button" className="btn btn-danger " onClick={ () => this.handleDelete( elm.id)}>Supprimer</button></td>
-                <td><button type="button" className="btn btn-warning " onClick={ () => this.abandon( elm.id)}>Signal d'Abandon</button></td>
-                </tr>
-            
-        )
-    })
-    return(
-        <div>
-            <table>
+            )
+        })
+
+  return (
+    <div>
+         <table>
             <tr>
                 <th>Nom</th>
                 <th>Prénom</th>
             </tr>
-            {mapped}
+           {mapped}
             </table>
-        </div>
-        
-    ) 
-    
-    
-  }
+      <h2>Adresse de l'étudiant :</h2>
+      <ul>
+        {etudiantAdress.map(adress => (
+          <li key={adress.Id}>
+            <p>Ville : {adress.Adress_City}</p>
+            <p>Pays : {adress.Adress_Country}</p>
+            <p>Quartier : {adress.Adress_Quartier}</p>
+            <p>Code postal : {adress.Code_Postal}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+  
+                // <td><button type="button" className="btn btn-danger " onClick={ () => this.handleDelete( elm.id)}>Supprimer</button></td>
+                // <td><button type="button" className="btn btn-warning " onClick={ () => this.abandon( elm.id)}>Signal d'Abandon</button></td>
+             
 
 export default StudentList
